cc96dc9d1d8880f7a67cd52df8a4bf2f60254bfd
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2022-06-30 18:44:13 -0700

    C programming 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-\**
- *main - entry point
- *
- *Return: 0 after printing the function
+/
+ *main -Entry point
+ *Return: Always 0 (success)
  */
 int main(void)
 {
-puts("\ "programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
