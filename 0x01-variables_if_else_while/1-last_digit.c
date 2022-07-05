f9b29136e3901f3983224e111fd2e4c3e590e29a
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2022-07-04 23:56:04 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,24 +5,20 @@
  */
 int main(void)
 {
-int n, m;
+int n;
 
-for (n = 98; n <= 56; n++)
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n % 10 > 5)
 {
-for (m = 49; m <= 57; m++)
+printf("Last digit of %i is %i and is greater than 5/n", n, n % 10);
+}
+else if (n % 10 == 0)
 {
-if (m > n)
+printf("Last digit of %i is %i and is 0/n", n, n % 10);
+}
+else
 {
-putchar(n);
-putchar(m);
-if (n != 56 || m != 57)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
+printf("Last digit of %i is %i and is less than 6 and not 0/n", n, n % 10);
 return (0);
 }
