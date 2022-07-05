a04ceb53726f9e06daecb9aeead81bc5f68c1a68
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2022-07-04 18:48:14 -0700

    task 1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
+ * main - prints all possible different combinations of two digits
  * Return: Always 0 (Success/completed)
  */
+
 int main(void)
+
 {
-int n,m;
+int n, m;
 
 for (n = 48; n <= 56; n++)
 {
