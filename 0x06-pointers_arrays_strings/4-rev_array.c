ec4808579e3432d1c7cf88c0b5e1f6e64a33720e
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2022-07-14 05:10:49 -0700

    more pointers 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reverse_array - a function that reverse an array.
- * @a: an input array
- * @n: number of elements of array
- * Return: Nothing
- */
+* reverse_array - a function that reverse an array.
+* @a: an input array
+* @n: number of elements of array
+* Return: Nothing
+*/
 void reverse_array(int *a, int n)
 {
 	int i = 0;
