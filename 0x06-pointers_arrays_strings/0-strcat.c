b05ae432af243800d16730bf26635503b29ff3ff
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2022-07-14 11:54:18 -0700

    more pointers 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* *_strcat - concatenates two strings
-* @dest: pointer destination
-* @src: pointer source
-* Return: void 
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
 */
 
 char *_strcat(char *dest, char *src)
@@ -13,9 +13,9 @@ for (i = 0; dest[i] != '\0'; i++)
 ;
 
 do {
-a++;
-dest[i] = src[a];
-i++;
+	a++;
+	dest[i] = src[a];
+	i++;
 } while (src[a] != '\0');
 
 return (dest);
