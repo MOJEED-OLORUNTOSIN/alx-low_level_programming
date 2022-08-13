97918cf7a083cd8032b7bb995230f9843e8699b0
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2022-08-13 16:07:20 -0700

    singly 0

--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,1 +1,1 @@
-singly-linked
+0x12-singly_linked_lists
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,5 @@
-#ifndef _LISTS_\
-#define _LISTS_
-
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
 #include <string.h>
 
@@ -11,11 +9,13 @@
  * @len: length of the string
  * @next: points to the next node
  * Description: singly linked list node structure
- * for Alx-low-project
+ * for Holberton project
  */
 
 typedef struct list_s
+
 {
+
 char *str;
 unsigned int len;
 struct list_s *next;
