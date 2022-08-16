6bef0a4a42c24e431eadb6639f27f42746aa0f2c
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2022-08-16 12:32:44 -0700

    more singly 100

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,33 +1,25 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint_safe - A function that prints the elementsin a  list
- * @head: A pointer to listint_t structure
- * Return: The number of nodes. Exits with 98 on failure
+ * reverse_listint - Reverses a listint_t list.
+ * @head: A pointer to the address of
+ * the head of the list_t list.
+ * Return: A pointer to the first node of the reversed list.
  */
-size_t print_listint_safe(const listint_t *head)
+
+listint_t *reverse_listint(listint_t **head)
 {
-	size_t nodes = 0;
-	const listint_t *one = head, *two = head;
-
-	if (head == NULL)
-		exit(98);
-
-	while (one && two && two->next && head)
-	{
-		one = one->next;
-		two = two->next->next;
-		if (one == two)
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
-		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nodes++;
-	}
-	head = NULL;
-	return (nodes);
+listint_t *ahead, *behind;
+if (head == NULL || *head == NULL)
+return (NULL);
+behind = NULL;
+while ((*head)->next != NULL)
+{
+ahead = (*head)->next;
+(*head)->next = behind;
+behind = *head;
+*head = ahead;
+}
+(*head)->next = behind;
+return (*head);
 }
